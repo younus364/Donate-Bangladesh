92983a0b529ab09253d2a2a1a2e6fad7c31a6f23
younus364 <younussheikh364@gmail.com>
2025-01-30 13:14:45 +0600

    Feni Add Function

--- js/setInput.js
+++ js/setInput.js
@@ -36,3 +36,32 @@ document.getElementById('noakhali').addEventListener('click',function(){
     
     historyList('donet-list')
  })
+
+
+ document.getElementById('feni').addEventListener('click',function(){
+   const donatedTaka = calculation('feni-input')
+   if(isNaN(donatedTaka)){
+      return ;
+      
+    }
+   const rechentBdt = setInnerText('feni-BDT')
+   const totalBDT = setInnerText('inTotalBDT')
+   const result = donatedTaka + rechentBdt;
+   const total = totalBDT - result;
+   document.getElementById('feni-BDT').innerText = result;
+   document.getElementById('inTotalBDT').innerText = total;
+   document.getElementById('feni-input').value = ' '
+
+   const today = new Date();
+   const dateString = today.toLocaleDateString();
+   console.log(dateString);
+   const div = document.createElement('div')
+   div.innerHTML = `
+   <div class = " border rounded-lg p-6 mb-3 ">
+   <h1 class = "text-xl font-bold mb-3"> ${donatedTaka} Taka is Donate for Flood Relief in Feni,Bangladesh </h1>
+   <p class = " bg-slate-100 p-2 rounded-md"> ${today}</p>
+    </div>
+   `
+  document.getElementById('donet-list').appendChild(div)
+})
+ 
